63a3a1618880cde4b7d6fc179659b29bdd8e4445
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2016-02-04 09:47:27 +0100

    [OrganizeTab] Layer Detail : implemented ZoomToExtent & Show more (metadata)
    - Metadata opens in an external browser window
    
    TODO : group the handling of button events, cleaner than that

--- fr.pigeo.rimap.rimaprcp/src/fr/pigeo/rimap/rimaprcp/swt/LayerDetails.java
+++ fr.pigeo.rimap.rimaprcp/src/fr/pigeo/rimap/rimaprcp/swt/LayerDetails.java
@@ -10,12 +10,8 @@ import org.eclipse.core.databinding.beans.PojoProperties;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.jface.databinding.swt.WidgetProperties;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.ScrolledComposite;
-import org.eclipse.swt.events.ControlAdapter;
-import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -23,22 +19,25 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Scale;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.wb.swt.ResourceManager;
 import org.eclipse.wb.swt.SWTResourceManager;
 
 import fr.pigeo.rimap.rimaprcp.swt.bindings.OpacityToScaleConverter;
 import fr.pigeo.rimap.rimaprcp.swt.bindings.ScaleToOpacityConverter;
 import gov.nasa.worldwind.layers.Layer;
+import org.eclipse.swt.events.SelectionAdapter;
 
 public class LayerDetails {
 	protected DataBindingContext m_bindingContext;
 	protected Layer layer;
 	
-	protected Label lblNoLayer, lblLayerName,lblLayerDescription,lblOpacity;
+	protected Label lblNoLayer, lblLayerName,lblOpacity,lblDescription;
 	protected Scale scaleOpacity;
 	protected Group grpDetails;
-	protected ScrolledComposite scrolledCompositeDescriptionContainer;
 	protected Button btnShowMetadata, btnShowLegend;
+	protected Text txtLayerDescription;
+	protected Button btnZoomToExtent;
 
 	@Inject
 	public LayerDetails() {
@@ -65,15 +64,21 @@ public class LayerDetails {
 		lblNoLayer.setText("No Layer selected. Please select a layer to view its related details.");
 
 		lblLayerName = new Label(grpDetails, SWT.WRAP);
-		GridData gd_lblLayerName = new GridData(SWT.FILL, SWT.TOP, true, false, 2, 1);
+		GridData gd_lblLayerName = new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1);
 		gd_lblLayerName.minimumWidth = 200;
 		lblLayerName.setLayoutData(gd_lblLayerName);
 		lblLayerName.setFont(SWTResourceManager.getFont("Sans", 10, SWT.BOLD));
 		lblLayerName.setText("Layer name");
-
-		lblOpacity = new Label(grpDetails, SWT.NONE);
-		lblOpacity.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
-		lblOpacity.setText("Opacity:");
+				
+				btnZoomToExtent = new Button(grpDetails, SWT.NONE);
+				
+				btnZoomToExtent.setToolTipText("Zoom to layer's extent");
+				btnZoomToExtent.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+				btnZoomToExtent.setImage(ResourceManager.getPluginImage("fr.pigeo.rimap.rimaprcp", "icons/icon_zoomlayer.png"));
+		
+				lblOpacity = new Label(grpDetails, SWT.NONE);
+				lblOpacity.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+				lblOpacity.setText("Opacity:");
 		new Label(grpDetails, SWT.NONE);
 
 		scaleOpacity = new Scale(grpDetails, SWT.NONE);
@@ -93,31 +98,20 @@ public class LayerDetails {
 
 			}
 		});
+		
+		lblDescription = new Label(grpDetails, SWT.NONE);
+		lblDescription.setText("Description:");
 		new Label(grpDetails, SWT.NONE);
-		new Label(grpDetails, SWT.NONE);
-
-		scrolledCompositeDescriptionContainer = new ScrolledComposite(grpDetails, SWT.BORDER | SWT.V_SCROLL);
-		scrolledCompositeDescriptionContainer.setExpandHorizontal(true);
-		scrolledCompositeDescriptionContainer.setExpandVertical(true);
-		scrolledCompositeDescriptionContainer.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
-		scrolledCompositeDescriptionContainer.addControlListener(new ControlAdapter() {
-			public void controlResized(ControlEvent e) {
-				Rectangle r = scrolledCompositeDescriptionContainer.getClientArea();
-				scrolledCompositeDescriptionContainer.setMinSize(grpDetails.computeSize(r.width, SWT.DEFAULT));
-			}
-		});
-
-		lblLayerDescription = new Label(scrolledCompositeDescriptionContainer, SWT.WRAP);
-		lblLayerDescription.setFont(SWTResourceManager.getFont("Sans", 8, SWT.ITALIC));
-		lblLayerDescription.setText(
-				"R É S U M É\n\n1ÈRE PARTIE : PRINCIPES DIRECTEURS\n\nIntroduction\n\nObjectifs et structure\n\nLa rédaction de directives pour des technologies et ap-proches de gestion durable des terres en Afrique subsaha-rienne (ASS) fait partie du programme TerrAfrica de 2009- 2010. L’objectif de ces recommandations et études de cas est de contribuer à créer une cadre pour les investis-sements liés aux pratiques de gestion durable des terres (GDT). Le but est, en particulier, d’identifier, d’analyser, de discuter et de diffuser des pratiques de GDT prometteuses – incluant à la fois les technologies et les approches – à la lumière des dernières tendances et nouvelles opportuni-tés. L’étude cible surtout les pratiques qui produisent des résultats et un retour sur investissement rapides et / ou les autres facteurs qui incitent à l’adoption de ces pratiques.\n\nCe document s’adresse aux parties-prenantes clés des programmes et projets de GDT aux stades de l’élaboration et de la mise en oeuvre : il s’agit surtout des praticiens, des gestionnaires, des décideurs, des planificateurs, en collaboration avec les institutions financières et tech¬niques et les donateurs. Les directives sont divisées en deux parties principales. La 1ère partie met en lumière les grands principes de la GDT ainsi que les éléments impor-tants à prendre en compte qui permettront de qualifier les technologies et approches de « bonnes pratiques » pour une transposition à grande échelle. La 2ème partie pré-sente douze groupes de technologies de GDT ainsi qu’un module sur les approches de GDT. Celles-ci sont illus¬trées par des études de cas spécifiques. Les principales personnes ressources et experts en GDT en ASS ont été sollicités afin de finaliser les groupes de GDT et de décrire les études de cas spécifiques. Ce produit s’efforce d’être à la pointe de la recherche.\n\nFocus sur la gestion durable des terres en Afrique subsaharienne");
-		scrolledCompositeDescriptionContainer.setContent(lblLayerDescription);
-		scrolledCompositeDescriptionContainer.setMinSize(lblLayerDescription.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		
+		txtLayerDescription = new Text(grpDetails, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP | SWT.V_SCROLL | SWT.MULTI);
+		txtLayerDescription.setBackground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_LIGHT_SHADOW));
+		txtLayerDescription.setText("gridData.horizontalSpan = 3;\n    gridData.grabExcessVerticalSpace = true;\n\n    bookAbstract.setLayoutData(gridData);\n\n    // Button.\n    Button enter = new Button(shell, SWT.PUSH);\n    enter.setText(\"Enter\");\n\n    gridData = new GridData();\n    gridData.horizontalSpan = 4;\n    gridData.horizontalAlignment = GridData.END;\n    enter.setLayoutData(gridData);\n\n    // Fill information.\n\n    title.setText(\"Professional Java Interfaces with SWT/JFace\");\n    authors.setText(\"Jack Li Guojie\");\n    pages.setText(\"500pp\");\n    pubisher.setText(\"John Wiley & Sons\");\n    cover.setBackground(new Image(display, \"java2s.gif\"));\n    bookAbstract.setText(\n      \"This book provides a comprehensive guide for \\n\"\n        + \"you to create Java user interfaces with SWT/JFace. \");\n\n    shell.pack();\n    shell.open();");
+		txtLayerDescription.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, true, 2, 1));
 
 		btnShowMetadata = new Button(grpDetails, SWT.NONE);
 		btnShowMetadata
-				.setImage(ResourceManager.getPluginImage("fr.pigeo.rimap.rimaprcp", "icons/icon_featureinfo_16px.png"));
-		btnShowMetadata.setToolTipText("Show the associated metadata (information sheet about the data)");
+				.setImage(ResourceManager.getPluginImage("fr.pigeo.rimap.rimaprcp", "icons/icon_metadata_16px.png"));
+		btnShowMetadata.setToolTipText("Show the associated metadata (information sheet about the data)\nOpens a window in your favorite browser.");
 		btnShowMetadata.setLayoutData(new GridData(SWT.RIGHT, SWT.FILL, false, false, 1, 1));
 		btnShowMetadata.setText(" - Show more");
 
